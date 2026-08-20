10faa6be277edc55b35c15d047639992bdfb1879
Kaja Wroblewska <140711313+kajusia1@users.noreply.github.com>
2026-08-20 15:02:50 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from scipy.spatial import Delaunay
 from shapely.geometry import Polygon, MultiPolygon
 from shapely.ops import unary_union
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
 import xml.etree.ElementTree as ET
 import math
 import os
@@ -111,8 +114,121 @@ def get_cords(rat, slice_group, slice_id):
 # ==========================================
 # 2. GENEROWANIE GEOMETRII 2D I 3D
 # ==========================================
+# ==========================================
+# 2. GENEROWANIE GEOMETRII 2D I 3D
+# ==========================================
 
-def delulu_3d_slice(rat, slice_group, slice, max_edge_length=0.2, buffer_size=0.1):
+def delulu_pax(rat, slice_group, slice_id, max_edge_length=0.4, buffer_size=0.4, x_limits=[-8,8], y_limits=[-11,0], see_neurons=False, target_point=None, show_legend=False):
+    cords = get_cords(rat=rat, slice_group=slice_group, slice_id=slice_id)
+    if not cords:
+        return None
+
+    x_new = []
+    y_new = []
+    bregma = cords[1][2][0]
+
+    for marker, (x, y, z) in cords.items():
+        if marker in [1, 2, 7]:
+            x_new = np.concatenate([x_new, x])
+            y_new = np.concatenate([y_new, y])
+
+    if len(x_new) < 3:
+        return None
+
+    points = np.array(list(zip(x_new, y_new)), dtype=np.float32)
+
+    tri = Delaunay(points)
+    p = points[tri.simplices]
+    d01 = np.linalg.norm(p[:, 0] - p[:, 1], axis=1)
+    d12 = np.linalg.norm(p[:, 1] - p[:, 2], axis=1)
+    d20 = np.linalg.norm(p[:, 2] - p[:, 0], axis=1)
+
+    mask = (
+        (d01 < max_edge_length)
+        & (d12 < max_edge_length)
+        & (d20 < max_edge_length)
+    )
+    filtered_triangles = tri.simplices[mask]
+
+    point_color = "darkblue"
+    line_color = "darkblue"
+
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['font.sans-serif'] = ['Arial', 'Helvetica', 'DejaVu Sans']
+
+    fig, ax = plt.subplots(figsize=(6, 5), facecolor="white")
+    ax.set_facecolor("white")
+
+    img_path = rf"paxinos\{bregma}.png"
+    if os.path.exists(img_path):
+        img = plt.imread(img_path)
+        ax.imshow(img, extent=[-8, 8, -11, 0], aspect="equal", zorder=0)
+
+    ax.tick_params(
+        axis='both',
+        which='major',
+        labelsize=10,
+        colors='#222222',
+        direction='out',
+        length=4,
+        width=1.0
+    )
+
+    if len(filtered_triangles) > 0:
+        polygons = [Polygon(points[tri_idx]) for tri_idx in filtered_triangles]
+        blob_poly = unary_union(polygons)
+        smooth_blob = blob_poly.buffer(buffer_size).buffer(-buffer_size)
+
+        if isinstance(smooth_blob, Polygon):
+            geometries = [smooth_blob]
+        elif isinstance(smooth_blob, MultiPolygon):
+            geometries = list(smooth_blob.geoms)
+        else:
+            geometries = []
+
+        for poly in geometries:
+            if poly.geom_type == "Polygon":
+                xs, ys = poly.exterior.xy
+                ax.fill(xs, ys, color=line_color, alpha=0.1, zorder=1)
+                ax.plot(xs, ys, color=line_color, linewidth=0.7, zorder=2)
+
+                for interior in poly.interiors:
+                    ixs, iys = interior.xy
+                    ax.fill(ixs, iys, color="white", zorder=2)
+                    ax.plot(ixs, iys, color=line_color, linewidth=0.7, zorder=3)
+
+    if see_neurons:
+        ax.scatter(
+            points[:, 0],
+            points[:, 1],
+            color=point_color,
+            s=1,
+            alpha=0.8,
+            zorder=4,
+            edgecolors="none",
+        )
+
+    if target_point is not None:
+        ax.scatter([target_point[0]], [target_point[1]], color="orange", s=30, zorder=5, edgecolors="black", linewidth=0.5)
+
+    if show_legend:
+        legend_elements = [
+            Patch(facecolor='darkblue', edgecolor='darkblue', alpha=0.3, label='RMTg Area'),
+            Line2D([0], [0], marker='o', color='w', markerfacecolor='orange', markeredgecolor='black', markersize=4, label='Target (XY Projection)')
+        ]
+        ax.legend(handles=legend_elements, loc='upper right', bbox_to_anchor=(1.002, 0.917), frameon=True, framealpha=0.8, fontsize=7, handletextpad=0.3, borderpad=0.4, labelspacing=0.3)
+    if x_limits is not None:
+        ax.set_xlim(x_limits)
+    if y_limits is not None:
+        ax.set_ylim(y_limits)
+
+    # ax.set_title(f"Bregma: {bregma} (Group: {slice_group}, Slice: {slice_id})", fontsize=10, pad=10)
+    ax.set_aspect("equal")
+    plt.tight_layout()
+
+    return fig
+
+def delulu_3d_slice(rat, slice_group, slice, max_edge_length=0.4, buffer_size=0.4):
     cords = get_cords(rat=rat, slice_group=slice_group, slice_id=slice)
     if not cords:
         return None, None
@@ -178,13 +294,21 @@ def build_3d_mesh_data(rat, max_edge_length=0.2, buffer_size=0.1, max_3d_edge_le
         contour_color = 'darkblue'
 
     base_path = f'rat{rat}'
+    # fig = go.Figure()
+    
+    # slices_data = {}
+    # slice_traces = []
+
+    # if not os.path.exists(base_path):
+    #     return None, None, None
     fig = go.Figure()
     
     slices_data = {}
     slice_traces = []
+    slice_metadata = []
 
     if not os.path.exists(base_path):
-        return None, None, None
+        return None, None, None, None
 
     for slice_group_folder in os.listdir(base_path):
         group_path = os.path.join(base_path, slice_group_folder)
@@ -209,6 +333,12 @@ def build_3d_mesh_data(rat, max_edge_length=0.2, buffer_size=0.1, max_3d_edge_le
 
                 if blob_geometry is None:
                     continue
+
+                slice_metadata.append({
+                    "slice_group": slice_group,
+                    "slice_id": slice_id,
+                    "z": z_slice
+                })
 
                 if isinstance(blob_geometry, Polygon):
                     geometries = [blob_geometry]
@@ -289,10 +419,34 @@ def build_3d_mesh_data(rat, max_edge_length=0.2, buffer_size=0.1, max_3d_edge_le
             for face in faces:
                 face_counts[face] = face_counts.get(face, 0) + 1
 
-        boundary_faces = [
-                face for face, count in face_counts.items() 
-                if count == 1 and not (pair_pts[face[0], 2] == pair_pts[face[1], 2] == pair_pts[face[2], 2])
-            ]           
+            is_first_pair = idx == 0
+            is_last_pair = idx == len(sorted_z) - 2
+
+            boundary_faces = [
+                face for face, count in face_counts.items()
+                if count == 1 and (
+                    # zwykła powierzchnia boczna
+                    not (
+                        pair_pts[face[0], 2] ==
+                        pair_pts[face[1], 2] ==
+                        pair_pts[face[2], 2]
+                    )
+                    # płaska powierzchnia MIN Z
+                    or (
+                        is_first_pair and
+                        pair_pts[face[0], 2] == z1 and
+                        pair_pts[face[1], 2] == z1 and
+                        pair_pts[face[2], 2] == z1
+                    )
+                    # płaska powierzchnia MAX Z
+                    or (
+                        is_last_pair and
+                        pair_pts[face[0], 2] == z2 and
+                        pair_pts[face[1], 2] == z2 and
+                        pair_pts[face[2], 2] == z2
+                    )
+                )
+            ]          
 
         if boundary_faces:
             boundary_faces = np.array(boundary_faces)
@@ -337,10 +491,10 @@ def build_3d_mesh_data(rat, max_edge_length=0.2, buffer_size=0.1, max_3d_edge_le
             zaxis_title='Dorso-Ventral',
             aspectmode='data'
         ),
-        title=f'RMTg 3D Reconstruction - rat {rat}'
+        # title=f'RMTg 3D Reconstruction - rat {rat}'
     )
 
-    return fig, np.array(global_points), active_tetrahedrons
+    return fig, np.array(global_points), active_tetrahedrons, slice_metadata
 
 # ==========================================
 # 3. INTERFEJS UŻYTKOWNIKA (STREAMLIT UI)
@@ -349,17 +503,17 @@ def build_3d_mesh_data(rat, max_edge_length=0.2, buffer_size=0.1, max_3d_edge_le
 st.title("Verify Neuron Location in RMTg")
 
 st.sidebar.header("Model Settings")
-rat_id = st.sidebar.number_input("Rat ID", value=2, step=1, min_value=2, max_value=8)
+# rat_id = st.sidebar.number_input("Rat ID", value=2, step=1, min_value=2, max_value=8)
+rat_id =2
 
 view_map = {
-    "Mesh": 0,
-    "Mesh with slices": 1
+    "Mesh with slices": 1,
+    "Mesh": 0
 }
 selected_view_label = st.sidebar.selectbox("View Mode", options=list(view_map.keys()), index=0)
 view_mode = view_map[selected_view_label]
-
 with st.spinner("Data calibration and 3D model building..."):
-    fig, global_points, tetrahedrons = build_3d_mesh_data(rat=rat_id, view=view_mode)
+    fig, global_points, tetrahedrons, slice_meta = build_3d_mesh_data(rat=rat_id, view=view_mode)
 
 if fig is None or len(global_points) == 0:
     st.error(f"No data or reference file available for folder 'rat{rat_id}'")
@@ -367,7 +521,7 @@ else:
     st.sidebar.header("Target Neuron Coordinates")
     target_x = st.sidebar.number_input("Lateral-Medial (X)", value=0.0, format="%.3f")
     target_y = st.sidebar.number_input("Anterior-Posterior (Y)", value=-7.60, format="%.3f")
-    target_z = st.sidebar.number_input("Dorso-Ventral (Z)", value=-6.60, format="%.3f")
+    target_z = st.sidebar.number_input("Dorso-Ventral (Z)", value=-7.00, format="%.3f")
 
     target_point = (target_x, target_y, target_z)
     inside = is_point_in_tetrahedron(target_point, tetrahedrons, global_points)
@@ -381,13 +535,36 @@ else:
         p_color = "orange"
         p_name = "Target Neuron - OUTSIDE RMTg"
 
+    st.subheader("RMTg 3D Reconstruction - rat 2")
     fig_display = go.Figure(fig)
     fig_display.add_trace(go.Scatter3d(
         x=[target_x], y=[target_y], z=[target_z],
         mode='markers',
         marker=dict(size=8, color=p_color, symbol='circle'),
-        name= "Target Neuron",
+        name="Target Neuron",
         hoverinfo='name+x+y+z'
     ))
 
     st.plotly_chart(fig_display, use_container_width=True)
+
+    if slice_meta:
+        sorted_slices = sorted(slice_meta, key=lambda s: abs(s["z"] - target_z))
+        closest_slices = sorted_slices[:2]
+
+        st.subheader("Closest Coronal Slices (Atlas Projection)")
+        cols = st.columns(len(closest_slices))
+
+        for idx, s_info in enumerate(closest_slices):
+            with cols[idx]:
+                fig_2d = delulu_pax(
+                    rat=rat_id,
+                    slice_group=s_info["slice_group"],
+                    slice_id=s_info["slice_id"],
+                    max_edge_length=0.4,
+                    buffer_size=0.4,
+                    see_neurons=False,
+                    target_point=(target_x, target_y),
+                    show_legend=True
+                )
+                if fig_2d is not None:
+                    st.pyplot(fig_2d)
