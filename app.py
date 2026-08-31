c60e87a327e0928bf14ffa6c8d97b2c960c567c3
Kaja Wroblewska <140711313+kajusia1@users.noreply.github.com>
2026-08-31 13:42:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,12 @@ def get_markers_by_type(root, marker_number):
 
 def calibrate_with_rotation(root, ref_cor):
     cor_dict = {}
-    for i in [1, 2, 3, 4, 5, 6]:
+    for i in , 2, 3, 4, 5, 6]:
         x, y, z = get_markers_by_type(root, i)
         cor_dict[i] = (x, y, ref_cor[2])
 
     target_x3, target_y3 = ref_cor[0]
-    target_x4, target_y4 = ref_cor[1]
+    target_x4, target_y4 = ref_cor]
     target_y3 = -target_y3
     target_y4 = -target_y4
     target_z = ref_cor[2]
@@ -125,10 +125,10 @@ def delulu_pax(rat, slice_group, slice_id, max_edge_length=0.4, buffer_size=0.4,
 
     x_new = []
     y_new = []
-    bregma = cords[1][2][0]
+    bregma = cords][2][0]
 
     for marker, (x, y, z) in cords.items():
-        if marker in [1, 2, 7]:
+        if marker in , 2, 7]:
             x_new = np.concatenate([x_new, x])
             y_new = np.concatenate([y_new, y])
 
@@ -209,7 +209,7 @@ def delulu_pax(rat, slice_group, slice_id, max_edge_length=0.4, buffer_size=0.4,
         )
 
     if target_point is not None:
-        ax.scatter([target_point[0]], [target_point[1]], color="orange", s=30, zorder=5, edgecolors="black", linewidth=0.5)
+        ax.scatter([target_point[0]], [target_point]], color="orange", s=30, zorder=5, edgecolors="black", linewidth=0.5)
 
     if show_legend:
         legend_elements = [
@@ -238,7 +238,7 @@ def delulu_3d_slice(rat, slice_group, slice, max_edge_length=0.4, buffer_size=0.
     z_slice = None 
 
     for marker, (x, y, z) in cords.items():
-        if marker in [1, 2] and x:
+        if marker in [1, 2, 7] and x:
             x_new = np.concatenate([x_new, x])
             y_new = np.concatenate([y_new, y])
             z_slice = z[0]
